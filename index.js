ce987cf49589f35cb20b9d42e964567bec52c389
Mohit Tater <mohittater815@gmail.com>
2025-10-17 16:57:08 +0530

    c

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from /!",
+    timestamp: new Date(),
+  });
+});
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello from /api!",
+    timestamp: new Date(),
+  });
+});
 app.get("/api/data", (req, res) => {
   res.json({
     message: "Hello from Backend!",
